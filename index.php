901c2ddfae12cdc7f39002dcc134c9d57517a59a
rudig68 <rudig068@gmail.com>
2022-03-05 21:51:32 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
 <body>
   <h1>heloo word</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repudiandae corporis ratione praesentium quod pariatur impedit perspiciatis. Illum eveniet praesentium tenetur est exercitationem voluptatum perspiciatis quidem voluptates numquam. Qui, eveniet?</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
   
 </body>
 </html>
